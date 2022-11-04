e38b015d8dd5f31039271ee9f1455831ff266d05
muhammet <muhammet@MONSTER>
2022-11-04 22:46:10 +0300

    Add project files.

---
+++ Entities/Concrete/CargoInformation.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class CargoInformation:IEntity
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Countries Country { get; set; }
+        public string RecipientFirstName { get; set; }
+        public string RecipientLastName { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User: IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalId { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+
+    }
+}
